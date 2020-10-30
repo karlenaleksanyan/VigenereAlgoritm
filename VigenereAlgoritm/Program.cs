c11a2dfb5291178c71d6877161dbd43b1d1cd5fe
karlen.aleksanyan.1999 <karlen.aleksanyan.1999@gmail.com>
2020-10-30 21:16:04 +0400

    Add project files.

---
+++ VigenereAlgoritm/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using VigenereAlgoritm;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        var cipher = new VigenereCipher();
+        Console.Write("Enter your text: ");
+        var inputText = Console.ReadLine().ToUpper();
+        Console.Write("Enter your keyword: ");
+        var password = Console.ReadLine().ToUpper();
+        var encryptedText = cipher.Encrypt(inputText, password);
+        Console.WriteLine("Decoding: {0}", encryptedText);
+        Console.WriteLine("Encode: {0}", cipher.Decrypt(encryptedText, password));
+        Console.ReadLine();
+    }
+}
---
+++ VigenereAlgoritm/VigenereCipher.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VigenereAlgoritm
+{
+	class VigenereCipher
+	{
+        const string defaultAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        readonly string letters;
+
+        public VigenereCipher(string alphabet = null)
+        {
+            letters = string.IsNullOrEmpty(alphabet) ? defaultAlphabet : alphabet;
+        }
+
+        private string GetRepeatKey(string s, int n)
+        {
+            var p = s;
+            while (p.Length < n)
+			{
+                p += p; 
+            }
+
+            return p.Substring(0, n);
+        }
+
+        private string Vigenere(string text, string password, bool encrypting = true)
+        {
+            string gamma = GetRepeatKey(password, text.Length);
+            string retValue = "";
+            int q = letters.Length;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                int letterIndex = letters.IndexOf(text[i]);
+                int codeIndex = letters.IndexOf(gamma[i]);
+                if (letterIndex < 0)
+                {
+                    retValue += text[i];
+                }
+                else
+                {
+                    retValue += letters[(q + letterIndex + ((encrypting ? 1 : -1) * codeIndex)) % q].ToString();
+                }
+            }
+
+            return retValue;
+        }
+
+        public string Encrypt(string plainMessage, string password)
+            => Vigenere(plainMessage, password);
+
+
+
+        public string Decrypt(string encryptedMessage, string password)
+            => Vigenere(encryptedMessage, password, false);
+    }
+}
